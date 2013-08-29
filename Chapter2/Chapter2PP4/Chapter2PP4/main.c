b048139615212b1c51f622a8cd8ff6097c850fd0
Randy McMillan <randy.lee.mcmillan@gmail.com>
2013-08-29 11:38:01 -0400

    double returnCelcius(double fahrenheitTemp){          double temp = (fahrenheitTemp / 2.0);              printf("%lf\n",fahrenheitTemp);
                         return temp;
    
    }
    
    	modified:   Chapter2PP4/Chapter2PP4/main.c

--- Chapter2/Chapter2PP4/Chapter2PP4/main.c
+++ Chapter2/Chapter2PP4/Chapter2PP4/main.c
@@ -1,81 +1,38 @@
- //
- //  main.c
- //  Chapter2PP1
- //
- //  Created by Randy McMillan on 8/29/13.
- //  Copyright (c) 2013 Randy McMillan. All rights reserved.
- //
+//
+//  main.c
+//  Chapter2PP4
+//
+//  Created by Randy McMillan on 8/29/13.
+//  Copyright (c) 2013 Randy McMillan. All rights reserved.
+//
 
 #include <stdio.h>
-double calculateDistanceTraveled(double beginning, double ending);
+#include <math.h>
+double returnCelcius(double fahrenheitTemp);
 
-double calculateReimbersment(double milesTraveled);
-double calculateCelcius(double fahrenheit);
-
-int main(int argc, const char *argv[])
+int main(int argc, const char * argv[])
 {
-	double reimbursement	= 0.0,
-    beginning			= 0.100,
-    ending				= 0.0,
-    
-    
-    fahrenheit = 0.0,
-    celcius = 0.0,
-    
-    milesTraveled;
-    
-	// insert code here...
-	printf("MILEAGE REIMBERSMENT CALCULATOR\n");
-	printf("Enter beggining odometer reading=>\n");
-	scanf("%lf", &beginning);
-	printf("Enter ending odometer reading=>\n");
-	scanf("%lf", &ending);
-	milesTraveled = calculateDistanceTraveled(beginning, ending);
-	printf("You traveled %lf miles. At $0.35 per mile,\n", milesTraveled);
-	reimbursement = calculateReimbersment(milesTraveled);
-	printf("your reimbersment is $ %lf\n", reimbursement);
-    
-    
-    
-    
-    printf("Enter F\n");
-	scanf("%lf", &fahrenheit);
-	celcius = calculateCelcius(fahrenheit);
-	printf("calculateCelcius(fahrenheit) = %lf\n", celcius);
-    
-	return 0;
-}
 
-double calculateDistanceTraveled(double beginning, double ending)
-{
-	double temp = ending - beginning;
+    double fahrenheit = 0.0;
+    double celcius = 0.0;
     
-	return temp;
-}
-
-double calculateReimbersment(double milesTraveled)
-{
-	double tempReimbursment = milesTraveled * 0.35;
     
-	return tempReimbursment;
+    printf("Please enter the tempreature in degrees fahrenheit.\n");
+    scanf("%lf",&fahrenheit);
+    celcius = returnCelcius(fahrenheit);
+    printf("%lf",celcius);
+    
+    
+    // insert code here...
+    //printf("Hello, World!\n");
+    return 0;
 }
 
 
-double calculateCelcius(double fahrenheitTemp){
+double returnCelcius(double fahrenheitTemp){
+     double temp = (fahrenheitTemp / 2.0);
+    printf("%lf\n",fahrenheitTemp);
     
-    double temp1 = fahrenheitTemp - 32;
-    printf("%lf\n",temp1);
+    return temp;
     
-    double temp2 = 5.0 / 9.0;
-    
-    printf("%lf\n",temp2);
-    double temp3 = fahrenheitTemp * temp2;
-    
-    printf("%lf\n",temp3);
-    double celciusTemp = temp3;
-    
-    printf("%lf\n",celciusTemp);
-    return celciusTemp;
-    
-
 }
