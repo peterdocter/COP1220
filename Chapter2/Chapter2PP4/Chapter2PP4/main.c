0381d1d18d0114350b5b38ef4e1d0e1126afe91e
Randy McMillan <randy.lee.mcmillan@gmail.com>
2013-08-29 11:43:32 -0400

    ok working 	modified:   Chapter2PP4/Chapter2PP4/main.c

--- Chapter2/Chapter2PP4/Chapter2PP4/main.c
+++ Chapter2/Chapter2PP4/Chapter2PP4/main.c
@@ -8,31 +8,29 @@
 
 #include <stdio.h>
 #include <math.h>
+
+#define FIVE_NINTHS 5.0 / 9.0
+
 double returnCelcius(double fahrenheitTemp);
 
-int main(int argc, const char * argv[])
+int main(int argc, const char *argv[])
 {
+	double	fahrenheit	= 0.0;
+	double	celcius		= 0.0;
 
-    double fahrenheit = 0.0;
-    double celcius = 0.0;
-    
-    
-    printf("Please enter the tempreature in degrees fahrenheit.\n");
-    scanf("%lf",&fahrenheit);
-    celcius = returnCelcius(fahrenheit);
-    printf("%lf",celcius);
-    
-    
-    // insert code here...
-    //printf("Hello, World!\n");
-    return 0;
+	printf("Please enter the tempreature in degrees fahrenheit.\n");
+	scanf("%lf", &fahrenheit);
+	celcius = returnCelcius(fahrenheit);
+	printf("%lf degrees Fahrenheit is %lf Celsius",fahrenheit, celcius);
+
+	return 0;
 }
 
+double returnCelcius(double fahrenheitTemp)
+{
+	double temp = (FIVE_NINTHS * (fahrenheitTemp - 32));
 
-double returnCelcius(double fahrenheitTemp){
-     double temp = (fahrenheitTemp / 2.0);
-    printf("%lf\n",fahrenheitTemp);
-    
-    return temp;
-    
+	//printf("%lf\n", fahrenheitTemp);
+
+	return temp;
 }
