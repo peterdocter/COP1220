fd28e084c2cd50c993b78701e1e81b40b618c805
Randy McMillan <randy.lee.mcmillan@gmail.com>
2013-08-29 10:26:33 -0400

    new file:   Chapter2/Chapter2PP1/Chapter2PP1.xcodeproj/project.pbxproj 	new file:   Chapter2/Chapter2PP1/Chapter2PP1/Chapter2PP1.1 	new file:   Chapter2/Chapter2PP1/Chapter2PP1/main.c 	new file:   Chapter2/Chapter2PP4/Chapter2PP4.xcodeproj/project.pbxproj 	new file:   Chapter2/Chapter2PP4/Chapter2PP4/Chapter2PP4.1 	new file:   Chapter2/Chapter2PP4/Chapter2PP4/main.c

---
+++ Chapter2/Chapter2PP1/Chapter2PP1/main.c
@@ -0,0 +1,46 @@
+//
+//  main.c
+//  Chapter2PP1
+//
+//  Created by Randy McMillan on 8/29/13.
+//  Copyright (c) 2013 Randy McMillan. All rights reserved.
+//
+
+#include <stdio.h>
+double calculateDistanceTraveled(double beginning, double ending);
+
+double calculateReimbersment(double milesTraveled);
+
+int main(int argc, const char *argv[])
+{
+	double reimbursement	= 0.0,
+		beginning			= 0.100,
+		ending				= 0.0,
+		milesTraveled;
+
+	// insert code here...
+	printf("MILEAGE REIMBERSMENT CALCULATOR\n");
+	printf("Enter beggining odometer reading=>\n");
+	scanf("%lf", &beginning);
+	printf("Enter ending odometer reading=>\n");
+	scanf("%lf", &ending);
+	milesTraveled = calculateDistanceTraveled(beginning, ending);
+	printf("You traveled %lf miles. At $0.35 per mile,\n", milesTraveled);
+	reimbursement = calculateReimbersment(milesTraveled);
+	printf("your reimbersment is $ %lf\n", reimbursement);
+	return 0;
+}
+
+double calculateDistanceTraveled(double beginning, double ending)
+{
+	double temp = ending - beginning;
+
+	return temp;
+}
+
+double calculateReimbersment(double milesTraveled)
+{
+	double tempReimbursment = milesTraveled * 0.35;
+
+	return tempReimbursment;
+}
---
+++ Chapter2/Chapter2PP4/Chapter2PP4/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Chapter2PP4
+//
+//  Created by Randy McMillan on 8/29/13.
+//  Copyright (c) 2013 Randy McMillan. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
