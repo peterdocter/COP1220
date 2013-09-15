2cf7da68463321d8fe12567e8f6e40c876c5ab12
Randy McMillan <randy.lee.mcmillan@gmail.com>
2013-09-14 22:56:40 -0400

    modified:   Chapter3/Chpt3PP10/Chpt3PP10/main.c

--- Chapter3/Chpt3PP10/Chpt3PP10/main.c
+++ Chapter3/Chpt3PP10/Chpt3PP10/main.c
@@ -14,14 +14,22 @@ float calcFarenheit(float depth);
 int main(int argc, const char * argv[])
 {
 
+    printf("Please enter a depth");
+    float depth = 0;
+    scanf("%f", &depth);
+ 
+    float celcius = calcCelcius(depth);
+    
+    printf("It is %0.2f degrees Celcius at that depth.",celcius);
+    
     // insert code here...
-    printf("Hello, World!\n");
+    //printf("Hello, World!\n");
     return 0;
 }
 
 float calcCelcius(float depth){
 
-    float celcius = 0;
+    float celcius = 10*depth+20;
     
     return celcius;
 
@@ -30,7 +38,9 @@ float calcCelcius(float depth){
 float calcFarenheit(float depth){
     
     float farenheit = 0;
-
+    
+    
+    
     return farenheit;
 
 }
