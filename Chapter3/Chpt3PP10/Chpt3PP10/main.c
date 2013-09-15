d5bbd29fa22f837405668cc644c23e27ada493b5
Randy McMillan <randy.lee.mcmillan@gmail.com>
2013-09-14 22:47:08 -0400

    added Chpt3PP10

---
+++ Chapter3/Chpt3PP10/Chpt3PP10/main.c
@@ -0,0 +1,36 @@
+//
+//  main.c
+//  Chpt3PP10
+//
+//  Created by Randy McMillan on 9/14/13.
+//  Copyright (c) 2013 Randy McMillan. All rights reserved.
+//
+
+#include <stdio.h>
+float calcCelcius(float depth);
+float calcFarenheit(float depth);
+
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
+float calcCelcius(float depth){
+
+    float celcius = 0;
+    
+    return celcius;
+
+}
+
+float calcFarenheit(float depth){
+    
+    float farenheit = 0;
+
+    return farenheit;
+
+}
