63f9e9ab146f31e7ac4dce3ad01edc3dc89ae53b
Randy McMillan <randy.lee.mcmillan@gmail.com>
2013-09-27 10:47:26 -0400

    new file:   Chapter4/Chpt4PP2/Chpt4PP2.xcodeproj/project.pbxproj 	new file:   Chapter4/Chpt4PP2/Chpt4PP2/Chpt4PP2.1 	new file:   Chapter4/Chpt4PP2/Chpt4PP2/main.c 	new file:   fall2013.pdf

---
+++ Chapter4/Chpt4PP2/Chpt4PP2/main.c
@@ -0,0 +1,21 @@
+//
+//  main.c
+//  Chpt4PP2
+//
+//  Created by Randy McMillan on 9/26/13.
+//  Copyright (c) 2013 Randy McMillan. All rights reserved.
+//
+
+#include <stdio.h>
+
+
+float evalBMI(float weight,float height);
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    //printf("Hello, World!\n");
+    return 0;
+}
+
