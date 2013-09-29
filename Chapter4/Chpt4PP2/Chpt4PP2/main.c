57509e0c6e0fafd446d194ffbd4554739baa15ee
Randy McMillan <randy.lee.mcmillan@gmail.com>
2013-09-29 19:52:58 -0400

    modified:   Chapter4/Chpt4PP2/Chpt4PP2/main.c

--- Chapter4/Chpt4PP2/Chpt4PP2/main.c
+++ Chapter4/Chpt4PP2/Chpt4PP2/main.c
@@ -6,16 +6,79 @@
 //  Copyright (c) 2013 Randy McMillan. All rights reserved.
 //
 
+
+
+/*
+ 
+ 
+ 2. Write a program that calculates the user’s body mass index (BMI) and catego- rizes it as underweight, normal, overweight, or obese, based on the following table from the United States Centers for Disease Control:
+ BMI
+ Below 18.5 18.5–24.9 25.0–29.9
+ 30.0 and above
+ Weight Status
+ Underweight Normal Overweight Obese
+ Programming Projects 231
+ ￼￼￼To calculate BMI based on weight in pounds (wt_lb) and height in inches (ht_in), use this formula (rounded to tenths):
+ 703 * wt_lb ht_in2
+ Prompt the user to enter weight in pounds and height in inches.
+ 
+ 
+ 
+ */
 #include <stdio.h>
 
+float weight,height,bmi;
+float returnBMI(float w,float h);
+void calculate();
 
-float evalBMI(float weight,float height);
-
-int main(int argc, const char * argv[])
-{
-
-    // insert code here...
-    //printf("Hello, World!\n");
+int main(int argc, const char * argv[]){
+    
+    calculate();
     return 0;
+
 }
 
+
+
+void calculate(){
+
+    //double d;
+    //scanf("%lf", &d);
+    //printf ("%f", d);
+    
+    printf("Please enter the person's weight in pounds --> ");
+    scanf("%f",&weight);
+    printf("Please enter the person's height in inches --> ");
+    scanf("%f",&height);
+
+    bmi = returnBMI(weight, height);
+    printf("%f\n",bmi);
+    
+    if (bmi<18.5) {
+    
+        printf("The person's BMI = %f and is considered Underweight by the US CDC.\n",bmi);
+
+    } else if (bmi < 25) {
+    
+        printf("The person's BMI = %f and is considered Normal by the US CDC.\n",bmi);
+    
+    } else if (bmi < 30) {
+    
+        printf("The person's BMI = %f and is considered Overweight by the US CDC.\n",bmi);
+    
+    } else {
+    
+        printf("The person's BMI = %f and is considered Obese by the US CDC.\n",bmi);
+    
+    }
+    
+}
+
+
+//w weight
+//h height
+float returnBMI(float wt_lb, float ht_in){
+
+    return (703*wt_lb)/(ht_in*ht_in);
+
+}
