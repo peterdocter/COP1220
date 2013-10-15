82c5654c4950a832df4de82fd78c41a5275a7527
Randy McMillan <randy.lee.mcmillan@gmail.com>
2013-10-15 13:20:12 -0400

    new file:   Chapter6/Chpt6PP1/Chpt6PP1.xcodeproj/project.pbxproj 	new file:   Chapter6/Chpt6PP1/Chpt6PP1/Chpt6PP1.1 	new file:   Chapter6/Chpt6PP1/Chpt6PP1/main.c 	new file:   Chapter6/Chpt6PP3/Chpt6PP3.xcodeproj/project.pbxproj 	new file:   Chapter6/Chpt6PP3/Chpt6PP3/Chpt6PP3.1 	new file:   Chapter6/Chpt6PP3/Chpt6PP3/main.c 	modified:   cop1220schedule.pdf

---
+++ Chapter6/Chpt6PP1/Chpt6PP1/main.c
@@ -0,0 +1,28 @@
+//
+//  main.c
+//  Chpt6PP1
+//
+//  Created by Randy McMillan on 10/15/13.
+//  Copyright (c) 2013 Randy McMillan. All rights reserved.
+//
+
+
+/*
+ 
+ 
+ 1. Write a program for an automatic teller machine that dispenses money.
+ The user should enter the amount desired (a multiple of 10 dollars) and the machine dispenses this amount using the least number of bills. The bills dis- pensed are 50s, 20s, and 10s. Write a function that determines how many of each kind of bill to dispense.
+ 
+ 
+ */
+#include <stdio.h>
+
+void myMain();
+
+int main(int argc, const char * argv[])
+{
+
+    return 0;
+}
+
+void myMain(){}
---
+++ Chapter6/Chpt6PP3/Chpt6PP3/main.c
@@ -0,0 +1,27 @@
+//
+//  main.c
+//  Chpt6PP3
+//
+//  Created by Randy McMillan on 10/15/13.
+//  Copyright (c) 2013 Randy McMillan. All rights reserved.
+//
+
+
+/*
+ 
+ 
+ 3. Write a program to dispense change. The user enters the amount paid and the amount due. The program determines how many dollars, quarters, dimes, nickels, and pennies should be given as change. Write a function with four output parameters that determines the quantity of each kind of coin.
+ 
+ 
+ */
+#include <stdio.h>
+
+void myMain();
+
+int main(int argc, const char * argv[])
+{
+    
+    return 0;
+}
+
+void myMain(){}
