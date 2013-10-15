7408d6fd7873f3d25e414f83b40d96bf40dad3e2
Randy McMillan <randy.lee.mcmillan@gmail.com>
2013-10-15 19:38:22 -0400

    weird fives bug good

--- Chapter6/Chpt6PP1/Chpt6PP1/main.c
+++ Chapter6/Chpt6PP1/Chpt6PP1/main.c
@@ -6,53 +6,96 @@
 //  Copyright (c) 2013 Randy McMillan. All rights reserved.
 //
 
-
 /*
- 
- 
- 1. Write a program for an automatic teller machine that dispenses money.
- The user should enter the amount desired (a multiple of 10 dollars) and the machine dispenses this amount using the least number of bills. The bills dis- pensed are 50s, 20s, and 10s. Write a function that determines how many of each kind of bill to dispense.
- 
- 
+ *
+ *
+ *   1. Write a program for an automatic teller machine that dispenses money.
+ *   The user should enter the amount desired (a multiple of 10 dollars) and the machine dispenses this amount using the least number of bills. The bills dis- pensed are 50s, 20s, and 10s. Write a function that determines how many of each kind of bill to dispense.
+ *
+ *
  */
 #include <stdio.h>
 
-float amount;
-int fifties,twneties,tens,fives,ones;
-float cents;
+float	amount;
+int		fifties, twenties, tens, fives, ones;
+float	cents;
 
 void inputAmount();
-void parseBills();
+
+void parseBills(float);
 
 void myMain();
 
-int main(int argc, const char * argv[])
+int main(int argc, const char *argv[])
 {
-
-    myMain();
-    return 0;
+	myMain();
+	return 0;
 }
 
-void myMain(){
+void myMain()
+{
+	amount		= 0;
+	fifties		= 0;
+	twenties	= 0;
+	tens		= 0;
+	fives		= 0;
+	ones		= 0;
+	cents		= 0;
+	// printf("PLease...");
+	// scanf("%f",&amount);
+	// printf("amount %f",amount);
 
-
-    // printf("PLease...");
-    //scanf("%f",&amount);
-    //printf("amount %f",amount);
-
-    inputAmount();
-    
-    
+	inputAmount();
 }
 
-void inputAmount(){
+void inputAmount()
+{
+	printf("Please enter amount to be parsed --> ");
+	scanf("%f", &amount);
+	// printf("amount %.2f\n",amount);
+
+	parseBills(amount);
+}
+
+void parseBills(float a)
+{
+	// printf("parseBills\n");
+	// printf("%.2f\n",a);
+
+	if (a / 50 >= 1) {
+		fifties = a / 50;
+		printf("fifties = %i\n", fifties);
+		a = a - (fifties * 50);
+		printf("%f\n", a);
+	}
+    
+    if (a / 20 >= 1) {
+		twenties = a / 20;
+		printf("twenties = %i\n", twenties);
+		a = a - (twenties * 20);
+		printf("%f\n", a);
+	}
+    
+    if (a / 10 >= 1) {
+		tens = a / 10;
+		printf("tens = %i\n", tens);
+		a = a - (tens * 10);
+		printf("%f\n", a);
+	}
+    
+    if (a / 5 >= 1) {
+		fives = a / 5;
+		printf("fives = %i\n", fives);
+		a = a - (fives * 5);
+		printf("%f\n", a);
+	}
 
     
-    printf("Please enter amount to be parsed --> ");
-    scanf("%f",&amount);
-    printf("amount %.2f\n",amount);
-
     
-
-
+    if (a / 1 >= 1) {
+		ones = a / 1;
+		printf("ones = %i\n", ones);
+		a = a - (ones * 1);
+		printf("%f\n", a);
+	}
 }
