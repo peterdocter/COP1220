a1e383681a6d4dfb075805f3ca2eadd4051c28bc
Randy McMillan <randy.lee.mcmillan@gmail.com>
2013-10-15 20:44:21 -0400

    modified:   Chapter6/Chpt6PP3/Chpt6PP3/main.c

--- Chapter6/Chpt6PP3/Chpt6PP3/main.c
+++ Chapter6/Chpt6PP3/Chpt6PP3/main.c
@@ -6,22 +6,142 @@
 //  Copyright (c) 2013 Randy McMillan. All rights reserved.
 //
 
-
 /*
- 
- 
- 3. Write a program to dispense change. The user enters the amount paid and the amount due. The program determines how many dollars, quarters, dimes, nickels, and pennies should be given as change. Write a function with four output parameters that determines the quantity of each kind of coin.
- 
- 
+ *
+ *
+ *   3. Write a program to dispense change. The user enters the amount paid and the amount due. The program determines how many dollars, quarters, dimes, nickels, and pennies should be given as change. Write a function with four output parameters that determines the quantity of each kind of coin.
+ *
+ *
  */
+
 #include <stdio.h>
+#include <math.h>
+
+float	amount;
+int		hundreds, fifties, twenties, tens, fives, ones, quarters, dimes, nickels, pennies;
+float	cents;
+
+int *hundredDollarBills, *fiftyDollarBills, *twentyDollarBills, *tenDollarBills, *fiveDollarBills, *oneDollarBills, *quarterDollars, *tenthDollars, *twentiethDollars, *hundrethsDollars;
+
+float amountPaid, amountDue, difference, change;
+
+void inputDue();
+
+void inputPaid();
+
+void calcDifference(float d, float p);
+
+void calcChange(float a);
+
+void displayChange();
 
 void myMain();
 
-int main(int argc, const char * argv[])
+int main(int argc, const char *argv[])
 {
-    
-    return 0;
+	// pointer usage per chapter subject
+	// not really neccesary but...
+	hundredDollarBills	= &hundreds;
+	fiftyDollarBills	= &fifties;
+	twentyDollarBills	= &twenties;
+	tenDollarBills		= &tens;
+	fiveDollarBills		= &fives;
+	oneDollarBills		= &ones;
+	quarterDollars		= &quarters;
+	tenthDollars		= &dimes;
+	twentiethDollars	= &nickels;
+	hundrethsDollars	= &pennies;
+
+	myMain();
+	return 0;
 }
 
-void myMain(){}
+void myMain()
+{
+	inputDue();
+	inputPaid();
+	calcDifference(amountDue, amountPaid);
+	calcChange(difference);
+	displayChange(change);
+}
+
+void inputDue()
+{
+	printf("Please input the amount due ---> ");
+	scanf("%f", &amountDue);
+}
+
+void inputPaid()
+{
+	printf("Please input the amount paid ---> ");
+	scanf("%f", &amountPaid);
+}
+
+void calcDifference(float d, float p)
+{
+	difference = d - p;
+
+	if (d < p) {
+		// printf("differnece = %.2lf",difference);
+		printf("differnece = %.2lf\n", fabs(difference));
+		difference = fabs(difference);
+		printf("differnece = %.2lf\n", fabs(difference));
+	} else {
+		// a bit of error checking
+		printf("Please reenter the amounts.\n");
+		myMain();
+	}
+}
+
+void calcChange(float a)
+{
+	// a = fabsf(a);
+	// printf("calcChange\n");
+	// printf("%.2f\n",a);
+
+	if (a / 100 >= 1) {
+		*hundredDollarBills = a / 100;
+		printf("hundreds = %i\n", hundreds);
+		a = a - (*hundredDollarBills * 100);
+		printf("%f\n", a);
+	}
+
+	if (a / 50 >= 1) {
+		*fiftyDollarBills = a / 50;
+		printf("fifties = %i\n", fifties);
+		a = a - (*fiftyDollarBills * 50);
+		printf("%f\n", a);
+	}
+
+	if (a / 20 >= 1) {
+		*twentyDollarBills = a / 20;
+		printf("twenties = %i\n", twenties);
+		a = a - (*twentyDollarBills * 20);
+		printf("%f\n", a);
+	}
+
+	if (a / 10 >= 1) {
+		*tenDollarBills = a / 10;
+		printf("tens = %i\n", tens);
+		a = a - (*tenDollarBills * 10);
+		printf("%f\n", a);
+	}
+
+	if (a / 5 >= 1) {
+		*fiftyDollarBills = a / 5;
+		printf("fives = %i\n", fives);
+		a = a - (*fiftyDollarBills * 5);
+		printf("%f\n", a);
+	}
+
+	if (a / 1 >= 1) {
+		*oneDollarBills = a / 1;
+		printf("ones = %i\n", ones);
+		a = a - (*oneDollarBills * 1);
+		printf("%f\n", a);
+	}
+
+	if (a / 1 < 1) {}
+}
+
+void displayChange() {}
