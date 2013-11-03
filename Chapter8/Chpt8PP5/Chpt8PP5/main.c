ad2511b8b901c0d4ad0d7937c35b165b10cd9e4b
Randy McMillan <randy.lee.mcmillan@gmail.com>
2013-11-03 14:52:24 -0500

    new file:   Chapter8/Chpt8PP1/Chpt8PP1.xcodeproj/project.pbxproj 	new file:   Chapter8/Chpt8PP1/Chpt8PP1/Chpt8PP1.1 	new file:   Chapter8/Chpt8PP1/Chpt8PP1/main.c 	new file:   Chapter8/Chpt8PP5/Chpt8PP5.xcodeproj/project.pbxproj 	new file:   Chapter8/Chpt8PP5/Chpt8PP5/Chpt8PP5.1 	new file:   Chapter8/Chpt8PP5/Chpt8PP5/main.c 	new file:   Chapter8/RandyMcMillanChpt8.docx 	new file:   Chapter8/Screen Shot 2013-11-03 at 2.43.02 PM.png 	new file:   Chapter8/Screen Shot 2013-11-03 at 2.43.49 PM.png 	new file:   Chapter8/Screen Shot 2013-11-03 at 2.45.43 PM.png 	new file:   Chapter8/Screen Shot 2013-11-03 at 2.45.52 PM.png 	new file:   Chapter8/Screen Shot 2013-11-03 at 2.51.17 PM.png 	new file:   Chapter8/Screen Shot 2013-11-03 at 2.51.26 PM.png 	new file:   Chapter8/Screen Shot 2013-11-03 at 2.51.33 PM.png 	new file:   Chapter8/Screen Shot 2013-11-03 at 2.51.40 PM.png 	modified:   Problem Solving and Program Design in C, Jeri R. Hanly - Elliot B. Koffman, 7th Edition, Pearson, 2013.pdf

---
+++ Chapter8/Chpt8PP5/Chpt8PP5/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Chpt8PP5
+//
+//  Created by Randy McMillan on 11/3/13.
+//  Copyright (c) 2013 Randy McMillan. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
