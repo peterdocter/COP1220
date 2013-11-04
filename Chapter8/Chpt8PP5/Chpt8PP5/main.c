ddd10a42343720f934277c1d45cc9dbf804dbecf
Randy McMillan <randy.lee.mcmillan@gmail.com>
2013-11-04 11:08:47 -0500

    modified:   Chapter8/Chpt8PP5/Chpt8PP5/main.c

--- Chapter8/Chpt8PP5/Chpt8PP5/main.c
+++ Chapter8/Chpt8PP5/Chpt8PP5/main.c
@@ -6,6 +6,22 @@
 //  Copyright (c) 2013 Randy McMillan. All rights reserved.
 //
 
+/*
+ 
+ 
+ 5. Write a program that takes nouns and forms their plurals on the basis of these rules:
+ a. If noun ends in “y”, remove the “y” and add “ies”.
+ b. If noun ends in “s”, “ch”, or “sh”, add “es”.
+ c. In all other cases, just add “s”.
+ to quit => 6
+ to quit => 12 to quit => 0
+ to quit => -1
+ Print each noun and its plural. Try the following data:
+ chair dairy boss circus fly dog church clue dish
+ 
+ 
+ */
+
 #include <stdio.h>
 
 int main(int argc, const char * argv[])
