d11004915ab14bf60876fe65aa17c6eff1375fe4
Randy McMillan <randy.lee.mcmillan@gmail.com>
2013-11-05 18:24:00 -0500

    modified:   Chapter8/Chpt8PP5/Chpt8PP5/main.c

--- Chapter8/Chpt8PP5/Chpt8PP5/main.c
+++ Chapter8/Chpt8PP5/Chpt8PP5/main.c
@@ -7,51 +7,56 @@
 //
 
 /*
- 
- 
- 5. Write a program that takes nouns and forms their plurals on the basis of these rules:
- a. If noun ends in “y”, remove the “y” and add “ies”.
- b. If noun ends in “s”, “ch”, or “sh”, add “es”.
- c. In all other cases, just add “s”.
- to quit => 6
- to quit => 12 to quit => 0
- to quit => -1
- Print each noun and its plural. Try the following data:
- chair dairy boss circus fly dog church clue dish
- 
- 
+ *
+ *
+ *   5. Write a program that takes nouns and forms their plurals on the basis of these rules:
+ *   a. If noun ends in “y”, remove the “y” and add “ies”.
+ *   b. If noun ends in “s”, “ch”, or “sh”, add “es”.
+ *   c. In all other cases, just add “s”.
+ *   to quit => 6
+ *   to quit => 12 to quit => 0
+ *   to quit => -1
+ *   Print each noun and its plural. Try the following data:
+ *   chair dairy boss circus fly dog church clue dish
+ *
+ *
  */
 
 #include <stdio.h>
-#define ZERO 0
-#define LINE 80
+#define ZERO	0
+#define LINE	80
 
 void myMain();
-void promptInput();
-char *scanLine(char *dest, int dest_len);
-char string[LINE];
 
-int main(int argc, const char * argv[])
+void promptInput();
+
+char *scanLine(char *dest, int dest_len);
+
+char string[LINE];
+char modified[LINE];
+
+char pluralize(char string);
+
+int main(int argc, const char *argv[])
 {
-    myMain();
-    return 0;
+	myMain();
+	return 0;
 }
 
-void myMain(){
-    promptInput();
-    
-    scanLine(string, LINE);
-    
-printf("%lu\n",strlen(string));
- 
-    
-    for (int i = strlen(string); i<=strlen(string); i--) {
-        printf("%i",i);
-        
-    }
-        //printf("%c",string[0]);
+void myMain()
+{
+	promptInput();
+	scanLine(string, LINE);
     
     
+    strncpy(modified, string, 10);
+
+    
+    printf("%s",&modified[0]);
+	//printf("%lu\n", strlen(string));
+    //	for (int i = strlen(string); i <= strlen(string); i--) {
+        //	printf("%i", i);
+        //}
 }
 
 void promptInput()
@@ -63,20 +68,25 @@ void promptInput()
 char *scanLine(char *dest, int dest_len)
 {
 	int i, ch;
-    
+
 	i = ZERO;
-    
+
 	for (ch = getchar(); ch != '\n' && ch != EOF && i < dest_len - 1; ch = getchar()) {
 		dest[i++] = ch;
 	}
-    
+
 	dest[i] = '\0';
-    
+
 	while (ch != '\n' && ch != EOF) {
 		ch = getchar();
 	}
-    
+
 	return dest;
 }
 
+char pluralize(char string)
+{
+	char plural = ' ';
 
+	return plural;
+}
