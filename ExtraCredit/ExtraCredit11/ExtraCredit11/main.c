272a077f08228773e86f37ca757045f26869a1e5
Randy McMillan <randy.lee.mcmillan@gmail.com>
2013-11-08 16:03:23 -0500

    modified:   ExtraCredit/ExtraCredit11/ExtraCredit11/main.c 	new file:   sortsearch.pdf

--- ExtraCredit/ExtraCredit11/ExtraCredit11/main.c
+++ ExtraCredit/ExtraCredit11/ExtraCredit11/main.c
@@ -5,51 +5,99 @@
 //  Created by Randy McMillan on 11/2/13.
 //  Copyright (c) 2013 Randy McMillan. All rights reserved.
 //
+
 /*
- 
- 
- 11. Write a function that will merge the contents of two sorted (ascending order) arrays of type double values, storing the result in an array output parameter (still in ascending order). The function should not assume that both its input parameter arrays are the same length but can assume that one array does not contain two copies of the same value. The result array should also contain no duplicate values.
- First array
- 04
- Second array
- Result array
- Hint: When one of the input arrays has been exhausted, do not forget to copy the remaining data in the other array into the result array. Test your function with cases in which (1) the first array is exhausted first, (2) the second array is exhausted first, and (3) the two arrays are exhausted at the same time (i.e., they end with the same value). Remember that the arrays input to this func- tion must already be sorted.
- 
- 
+ *
+ *
+ *   11. Write a function that will merge the contents of two sorted (ascending order) arrays of type double values, storing the result in an array output parameter (still in ascending order). The function should not assume that both its input parameter arrays are the same length but can assume that one array does not contain two copies of the same value. The result array should also contain no duplicate values.
+ *   First array
+ *   04
+ *   Second array
+ *   Result array
+ *   Hint: When one of the input arrays has been exhausted, do not forget to copy the remaining data in the other array into the result array. Test your function with cases in which (1) the first array is exhausted first, (2) the second array is exhausted first, and (3) the two arrays are exhausted at the same time (i.e., they end with the same value). Remember that the arrays input to this func- tion must already be sorted.
+ *
+ *
  */
 
 #include <stdio.h>
 
-double firstArray[]={-10.5,-1.8,3.5,6.3,7.2};
-double secondArray[]={-1.8,3.1,6.3};
+double	firstArray[] = {-10.5, -1.8, 3.5, 6.3, 7.2};
+double	secondArray[] = {-1.8, 3.1, 6.3};
+double	thirdArray[];
 
-void sortAndMerge(double *fA,double *fB);
+void sortAndMerge(double *fA, double *fB);
 
-int main(int argc, const char * argv[])
+int main(int argc, const char *argv[])
 {
+	int j = 0;
 
-    sortAndMerge(firstArray, secondArray);
-    
-    
-    for (int i=0; i<sizeof firstArray / sizeof *firstArray; i++) {
-        //
-        printf("\nfirstArray[%i] = %lf\n",i,firstArray[i]);
-    }
-    
-    
-    for (int i=0; i<sizeof secondArray / sizeof *secondArray; i++) {
-        //
-        printf("\nsecondArray[%i] = %lf\n",i,secondArray[i]);
-    }
-    
-    
-    return 0;
+	sortAndMerge(firstArray, secondArray);
+
+	for (int i = 0; i < ((sizeof firstArray / sizeof *firstArray) | (sizeof secondArray / sizeof *secondArray)); i++) {
+		printf("\nfirstArray[%i]  = %.2lf\n", i, firstArray[i]);
+		printf("\nsecondArray[%i] = %.2lf\n", i, secondArray[i]);
+
+		// double	firstArray[] = {-10.5, -1.8, 3.5, 6.3, 7.2};
+		// double	secondArray[] = {-1.8, 3.1, 6.3};
+
+		if (firstArray[i] > secondArray[i]) {
+			thirdArray[j]	= firstArray[i];
+			thirdArray[j] = secondArray[i];
+            j++;
+            printf("j = %i",j);
+            
+		} else {
+			thirdArray[j]	= secondArray[i];
+			thirdArray[j] = firstArray[i];
+            j++;
+            printf("j = %i",j);
+
+            
+        }
+
+		// j++;
+
+		printf("\nthirdArray[%i] = %lf\n", j, thirdArray[j]);
+	}
+
+	for (int i = 0; i < sizeof secondArray / sizeof *secondArray; i++) {
+		// printf("\nsecondArray[%i] = %lf\n", i, secondArray[i]);
+	}
+
+	return 0;
 }
 
+void sortAndMerge(double *fA, double *fB) {}
 
-void sortAndMerge(double *fA,double *fB){
+void merge(int a[], int m, int b[], int n, int sorted[])
+{
+	int i, j, k;
 
+	j = k = 0;
 
-    
+	for (i = 0; i < m + n; ) {
+		if ((j < m) && (k < n)) {
+			if (a[j] < b[k]) {
+				sorted[i] = a[j];
+				j++;
+			} else {
+				sorted[i] = b[k];
+				k++;
+			}
 
+			i++;
+		} else if (j == m) {
+			for (; i < m + n; ) {
+				sorted[i] = b[k];
+				k++;
+				i++;
+			}
+		} else {
+			for (; i < m + n; ) {
+				sorted[i] = a[j];
+				j++;
+				i++;
+			}
+		}
+	}
 }
