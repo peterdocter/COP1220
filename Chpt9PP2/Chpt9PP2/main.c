562fcdd8438ed292cace96ecc168d44b326d95f1
Randy McMillan <randy.lee.mcmillan@gmail.com>
2013-11-12 19:20:01 -0500

    modified:   Chpt9PP2/Chpt9PP2/main.c

--- Chpt9PP2/Chpt9PP2/main.c
+++ Chpt9PP2/Chpt9PP2/main.c
@@ -22,12 +22,13 @@
 char	input[LINE];
 char	output[LINE];
 int num = 0;
+char *reverse_string(char *str);
 
 void myMain();
 
 void promptInput();
 
-int determinePalindrone(char *string);
+int determinePalindrone(char *string,int z);
 
 int StringIsPalindrome(const char* string);
 
@@ -46,11 +47,16 @@ void myMain()
 	promptInput();
 	deBlank(scanLine(input, LINE), output, (int)strlen(input) + 1);
 	//printf("\n%s\n", output);
-    printf("\n%i\n",determinePalindrone(output));
-    printf("bool = %i",StringIsPalindrome(output));
+    //printf("\n%i\n",determinePalindrone(output,(int)strlen(output)));
+    
+    int boolean = determinePalindrone(output, (int)strlen(output));
     
     
-    
+    if (boolean) {
+        
+        printf("%s is a Palindrone when it is de blanked and evaluated as --> \n%s\n",input,output);
+        
+    }else{}
 }
 
 void promptInput()
@@ -75,28 +81,83 @@ void deBlank(char *input, char *output, int loopLength)
 }
 
 
-int determinePalindrone(char *string){
+int determinePalindrone(char *string,int z){
     
-    
-    size_t sizeEquals = strlen(string);
-
-    long counter = counter + sizeEquals;
-    printf("counter = %ld",counter);
-    long length = strlen(string);
-    
-    if (1) {
-        //
-        printf("%s -> converted to %s length = %ld",input,string,length);
+    if (!strcmp(string, reverse_string(string))) {
+    //if (strcmp(string, reverse_string(string))) {
+    //I've tried multiples ways
         
+        /*
+         
+         What am I missing with this syntax?
+         the C reference states: 
+         
+         2.14.10 strcmp
+         
+         Declaration:
+         
+         int strcmp(const char *str1, const char *str2);
+         Compares the string pointed to by str1 to the string pointed to by str2.
+         Returns zero if str1 and str2 are equal. Returns less than zero or greater than zero if str1 is less than or greater than str2 respectively.
+         
+         What am I missing with the syntax?
+         This has been happening for several chapters and I can't get logic statments to evaluate they way they should no matter what kindo of conditions I make.
+         
+         */
+        
+        //printf("Why doesnt this work?");
+    
+    } else {
+    
+        //printf("I can't get any logic to work in my programs. It's like there is some kind of bug in Xcode or something. It's effecting my work. ");
+
     }
     
-    
-    //determinePalindrone(output);
-    
-    
-    return num;
+    return StringIsPalindrome(string);
 }
 
+
+
+
+///common reverse string technique
+char *reverse_string(char *str)
+{
+	char	temp;
+	size_t	len = strlen(str) - 1;
+	size_t	i;
+	size_t	k = len;
+    
+	for (i = 0; i < len; i++) {
+		temp	= str[k];
+		str[k]	= str[i];
+		str[i]	= temp;
+		k--;
+        
+		if (k == (len / 2)) {
+			break;
+		}
+	}
+    
+	return str;
+}
+
+
+//http://dystopiancode.blogspot.com/2012/07/palindrome-checking-algorithm-in-ansi-c.html
+int StringIsPalindrome(const char* string)
+{
+    long length = strlen(string);//get a string length and assign it to length
+    int i;//create a counter
+    int isPalindrome = 1;//like a boolean flag
+    for(i = 0; i<(length/2); i++)//for while i is half the length of length and increment by 1
+    {
+        if(string[i]!=string[length-i-1])//if string[i] doesnt equal string[length minus 1 minus 1]
+        {
+            isPalindrome = 0;//if each letter matches the isPalindrome is never set to zero
+            break;
+        }
+    }
+    return isPalindrome;//returns 1 if isPalindrome is not set to zero
+}
 
 // Based on Figure 8.15 in Book
 char *scanLine(char *dest, int dest_len)
@@ -119,31 +180,4 @@ char *scanLine(char *dest, int dest_len)
 }
 
 
-/*
- * Description:
- *  Checks if a string is a palindrome
- * Parameters:
- *  string - the string who is going to be verified
- * Returns:
- *  true - the string is a palindrome
- *  false - the string is not a palindrome
- */
-
-//Palindrone Test Sample
-//http://dystopiancode.blogspot.com/2012/07/palindrome-checking-algorithm-in-ansi-c.html
-int StringIsPalindrome(const char* string)
-{
-    long length = strlen(string);
-    int i;
-    int isPalindrome = 1;
-    for(i = 0; i<(length/2); i++)
-    {
-        if(string[i]!=string[length-i-1])
-        {
-            isPalindrome = 0;
-            break;
-        }
-    }
-    return isPalindrome;
-}
 
