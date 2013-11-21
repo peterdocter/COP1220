932affcc40b68955effae4ba99b7337cf53a8880
Randy McMillan <randy.lee.mcmillan@gmail.com>
2013-11-21 10:17:42 -0500

    modified:   Chapter10/Chpt10PP2/Chpt10PP2/main.c

--- Chapter10/Chpt10PP2/Chpt10PP2/main.c
+++ Chapter10/Chpt10PP2/Chpt10PP2/main.c
@@ -6,35 +6,34 @@
 //  Copyright (c) 2013 Randy McMillan. All rights reserved.
 //
 
-
 /*
- 
- 2. Define a structure type element_t to represent one element from the peri- odic table of elements. Components should include the atomic number (an integer); the name, chemical symbol, and class (strings); a numeric field for the atomic weight; and a seven-element array of integers for the number of electrons in each shell. The following are the components of an element_t structure for sodium.
- 11 Sodium Na alkali_metal 22.9898 2 8 1 0 0 0 0
- Define and test I/O functions scan_element and print_element.
- 
- 
+ *
+ *   2. Define a structure type element_t to represent one element from the peri- odic table of elements. Components should include the atomic number (an integer); the name, chemical symbol, and class (strings); a numeric field for the atomic weight; and a seven-element array of integers for the number of electrons in each shell. The following are the components of an element_t structure for sodium.
+ *   11 Sodium Na alkali_metal 22.9898 2 8 1 0 0 0 0
+ *   Define and test I/O functions scan_element and print_element.
+ *
+ *
  */
 #include <stdio.h>
 
 typedef struct {
-
-    int atmomicNum;
-    char elementName[80];
-    char chemicalSymbol[2];
-    char class[80];
-    int atomicWeight;
-    int shell1,shell2,shell3,shell4,shell5,shell6,shell7;
-
+	
+   	char	elementName[80];
+    int		atmomicNum;
+	char	chemicalSymbol[2];
+	char	class[80];
+	int		atomicWeight;
+	int		shell1, shell2, shell3, shell4, shell5, shell6, shell7;
+    
 } element_t;
 
+void myMain();
 
-
-int main(int argc, const char * argv[])
+int main(int argc, const char *argv[])
 {
 
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+    
+	return 0;
 }
 
+void myMain(){}
