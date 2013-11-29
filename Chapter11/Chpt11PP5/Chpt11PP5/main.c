8f58f5cee0bf7da7d8dbe1157dd7b3c319521314
Randy McMillan <randy.lee.mcmillan@gmail.com>
2013-11-28 21:51:30 -0500

    new file:   Chapter11/Chpt11PP5/Chpt11PP5.xcodeproj/project.pbxproj 	new file:   Chapter11/Chpt11PP5/Chpt11PP5/Chpt11PP5.1 	new file:   Chapter11/Chpt11PP5/Chpt11PP5/main.c

---
+++ Chapter11/Chpt11PP5/Chpt11PP5/main.c
@@ -0,0 +1,28 @@
+//
+//  main.c
+//  Chpt11PP5
+//
+//  Created by Randy McMillan on 11/28/13.
+//  Copyright (c) 2013 Randy McMillan. All rights reserved.
+//
+/*
+ 
+ 5. Write a program that takes words from a text file and prints each one on a separate line of an output file followed by the number of letters (alphabetic characters) in the word. Any leading or trailing punctuation marks should be removed from the word before it is printed. When all the text has been processed, display on the screen a count of the words in the file. Assume that words are groups of nonwhitespace characters separated by one or more whitespace characters.
+ 
+ 
+ */
+#include <stdio.h>
+
+void myMain();
+int main(int argc, const char * argv[])
+{
+    myMain();
+    return 0;
+}
+
+void myMain(){
+
+    
+    
+
+}
